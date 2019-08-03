dbbcf55e225347ca4f937894bbefdc19ddf58e89
Felix Klauke <info@felix-klauke.de>
2019-08-03 17:00:37 +0200

    Add null checks.

--- bukkit/src/main/java/de/felixklauke/chiara/bukkit/util/BukkitUtils.java
+++ bukkit/src/main/java/de/felixklauke/chiara/bukkit/util/BukkitUtils.java
@@ -1,5 +1,6 @@
 package de.felixklauke.chiara.bukkit.util;
 
+import java.util.Objects;
 import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -7,6 +8,8 @@ import org.bukkit.OfflinePlayer;
 public class BukkitUtils {
 
   public static UUID getUniqueIdByPlayerName(String playerName) {
+
+    Objects.requireNonNull(playerName, "Player name cannot be null.");
 
     OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerName);
     return offlinePlayer.getUniqueId();
--- bukkit/src/main/java/de/felixklauke/chiara/bukkit/util/ReflectionUtils.java
+++ bukkit/src/main/java/de/felixklauke/chiara/bukkit/util/ReflectionUtils.java
@@ -1,11 +1,15 @@
 package de.felixklauke.chiara.bukkit.util;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 public class ReflectionUtils {
 
   public static Object getFieldValue(Object object, String fieldName)
       throws IllegalAccessException, NoSuchFieldException {
+
+    Objects.requireNonNull(object, "Cannot extract value of null object.");
+    Objects.requireNonNull(fieldName);
 
     Class<?> objectClass = object.getClass();
     Field declaredField = objectClass.getDeclaredField(fieldName);
